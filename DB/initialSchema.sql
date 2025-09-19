06e170a0a960703ed7f7ff97a527913852d37520
Chandrahas Kantheti <chandra.kantheti@gmail.com>
2025-09-19 16:14:00 -0700

    v0.1 Database Folder Established with Initial Schema sql file

---
+++ DB/initialSchema.sql
@@ -0,0 +1,14 @@
+-- For this project, we can use th TimeScaleDB extension which
+-- allows for the time-series data to be stored more efficiently.
+
+-- Table for Robot Runs
+-- Essentailly a metadata table for each robot log run
+
+-- Table for logs
+-- Stores the raw parse log data (time-series data persay)
+
+-- Not finalized yet, but the table for summaries
+-- This is the NLP-generated summaries per run storage table
+
+-- Prob. a table for error-patterns.
+--This is to store any recurring error patterns we find for cross-referencing (Phase 4:Optional)
